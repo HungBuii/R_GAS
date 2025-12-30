a1877b35726c9344fd8ebd9fc7eae78862fc8bb3
Hungg Duy Bui <buiduyhungg2310@gmail.com>
2025-12-30 14:25:11 +0700

    create: Aura's weapon

--- Source/Aura/Public/Character/BaseCharacter.h
+++ Source/Aura/Public/Character/BaseCharacter.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright by Hung nee
 
 #pragma once
 
@@ -18,5 +18,8 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	
+	UPROPERTY(EditAnywhere, Category="Combat")
+	TObjectPtr<USkeletalMeshComponent> Weapon;
 
 };
