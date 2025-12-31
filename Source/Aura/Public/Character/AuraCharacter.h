1cced8b4524a8fe76fddb4e3ac3e99d3d4f40dd8
Hungg Duy Bui <buiduyhungg2310@gmail.com>
2025-12-31 16:45:21 +0700

    update: Aura Movement around

--- Source/Aura/Public/Character/AuraCharacter.h
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -14,4 +14,6 @@ class AURA_API AAuraCharacter : public ABaseCharacter
 {
 	GENERATED_BODY()
 	
+public:
+	AAuraCharacter();
 };
---
+++ Source/Aura/Public/Game/BaseGameMode.h
@@ -0,0 +1,17 @@
+// Copyright by Hung nee
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BaseGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API ABaseGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
